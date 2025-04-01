ea5da1b0b7a86a4b487006a29b52a5ec810570c0
Oscar Ahlén <github@ahlen.dev>
2025-04-01 11:25:59 +0200

    Add lualine theme

---
+++ lua/lualine/themes/tokyonight.lua
@@ -0,0 +1,79 @@
+if vim.o.background == "light" then
+    local palette = require("palette-light")
+    return {
+        normal = {
+            a = { bg = palette.blue, fg = palette.bg0 },
+            b = { bg = palette.bg4, fg = palette.fg0 },
+            c = { bg = palette.bg0, fg = palette.comment },
+        },
+        insert = {
+            a = { bg = palette.green, fg = palette.bg0 },
+            b = { bg = palette.bg4, fg = palette.fg0 },
+            c = { bg = palette.bg0, fg = palette.comment },
+        },
+        visual = {
+            a = { bg = palette.orange, fg = palette.bg0 },
+            b = { bg = palette.bg4, fg = palette.fg0 },
+            c = { bg = palette.bg0, fg = palette.comment },
+        },
+        replace = {
+            a = { bg = palette.red, fg = palette.bg0 },
+            b = { bg = palette.bg4, fg = palette.fg0 },
+            c = { bg = palette.bg0, fg = palette.comment },
+        },
+        command = {
+            a = { bg = palette.purple, fg = palette.bg0 },
+            b = { bg = palette.bg4, fg = palette.fg0 },
+            c = { bg = palette.bg0, fg = palette.comment },
+        },
+        terminal = {
+            a = { bg = palette.cyan, fg = palette.bg0 },
+            b = { bg = palette.bg4, fg = palette.fg0 },
+            c = { bg = palette.bg0, fg = palette.comment },
+        },
+        inactive = {
+            a = { bg = palette.fg0, fg = palette.bg0 },
+            b = { bg = palette.bg4, fg = palette.fg0 },
+            c = { bg = palette.bg0, fg = palette.comment },
+        },
+    }
+end
+
+local palette = require("palette-dark")
+return {
+    normal = {
+        a = { bg = palette.blue, fg = palette.bg0 },
+        b = { bg = palette.bg4, fg = palette.fg0 },
+        c = { bg = palette.bg0, fg = palette.comment },
+    },
+    insert = {
+        a = { bg = palette.green, fg = palette.bg0 },
+        b = { bg = palette.bg4, fg = palette.fg0 },
+        c = { bg = palette.bg0, fg = palette.comment },
+    },
+    visual = {
+        a = { bg = palette.orange, fg = palette.bg0 },
+        b = { bg = palette.bg4, fg = palette.fg0 },
+        c = { bg = palette.bg0, fg = palette.comment },
+    },
+    replace = {
+        a = { bg = palette.red, fg = palette.bg0 },
+        b = { bg = palette.bg4, fg = palette.fg0 },
+        c = { bg = palette.bg0, fg = palette.comment },
+    },
+    command = {
+        a = { bg = palette.purple, fg = palette.bg0 },
+        b = { bg = palette.bg4, fg = palette.fg0 },
+        c = { bg = palette.bg0, fg = palette.comment },
+    },
+    terminal = {
+        a = { bg = palette.cyan, fg = palette.bg0 },
+        b = { bg = palette.bg4, fg = palette.fg0 },
+        c = { bg = palette.bg0, fg = palette.comment },
+    },
+    inactive = {
+        a = { bg = palette.fg0, fg = palette.bg0 },
+        b = { bg = palette.bg4, fg = palette.fg0 },
+        c = { bg = palette.bg0, fg = palette.comment },
+    },
+}
