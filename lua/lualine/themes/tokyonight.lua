753366cb1725e8f96d179a567b4a608f8d78d46e
Oscar Ahlén <github@ahlen.dev>
2025-04-01 11:43:23 +0200

    Tweak colors

--- lua/lualine/themes/tokyonight.lua
+++ lua/lualine/themes/tokyonight.lua
@@ -3,38 +3,38 @@ if vim.o.background == "light" then
     return {
         normal = {
             a = { bg = palette.blue, fg = palette.bg0 },
-            b = { bg = palette.bg4, fg = palette.fg0 },
-            c = { bg = palette.bg0, fg = palette.comment },
+            b = { bg = palette.bg3, fg = palette.fg1 },
+            c = { bg = palette.bg0, fg = palette.fg0 },
         },
         insert = {
             a = { bg = palette.green, fg = palette.bg0 },
-            b = { bg = palette.bg4, fg = palette.fg0 },
-            c = { bg = palette.bg0, fg = palette.comment },
+            b = { bg = palette.bg3, fg = palette.fg1 },
+            c = { bg = palette.bg0, fg = palette.fg0 },
         },
         visual = {
             a = { bg = palette.orange, fg = palette.bg0 },
-            b = { bg = palette.bg4, fg = palette.fg0 },
-            c = { bg = palette.bg0, fg = palette.comment },
+            b = { bg = palette.bg3, fg = palette.fg1 },
+            c = { bg = palette.bg0, fg = palette.fg0 },
         },
         replace = {
             a = { bg = palette.red, fg = palette.bg0 },
-            b = { bg = palette.bg4, fg = palette.fg0 },
-            c = { bg = palette.bg0, fg = palette.comment },
+            b = { bg = palette.bg3, fg = palette.fg1 },
+            c = { bg = palette.bg0, fg = palette.fg0 },
         },
         command = {
             a = { bg = palette.purple, fg = palette.bg0 },
-            b = { bg = palette.bg4, fg = palette.fg0 },
-            c = { bg = palette.bg0, fg = palette.comment },
+            b = { bg = palette.bg3, fg = palette.fg1 },
+            c = { bg = palette.bg0, fg = palette.fg0 },
         },
         terminal = {
             a = { bg = palette.cyan, fg = palette.bg0 },
-            b = { bg = palette.bg4, fg = palette.fg0 },
-            c = { bg = palette.bg0, fg = palette.comment },
+            b = { bg = palette.bg3, fg = palette.fg1 },
+            c = { bg = palette.bg0, fg = palette.fg0 },
         },
         inactive = {
             a = { bg = palette.fg0, fg = palette.bg0 },
-            b = { bg = palette.bg4, fg = palette.fg0 },
-            c = { bg = palette.bg0, fg = palette.comment },
+            b = { bg = palette.bg3, fg = palette.fg1 },
+            c = { bg = palette.bg0, fg = palette.fg0 },
         },
     }
 end
@@ -43,37 +43,37 @@ local palette = require("tokyonight.palette-dark")
 return {
     normal = {
         a = { bg = palette.blue, fg = palette.bg0 },
-        b = { bg = palette.bg4, fg = palette.fg0 },
-        c = { bg = palette.bg0, fg = palette.comment },
+        b = { bg = palette.bg3, fg = palette.fg1 },
+        c = { bg = palette.bg0, fg = palette.fg0 },
     },
     insert = {
         a = { bg = palette.green, fg = palette.bg0 },
-        b = { bg = palette.bg4, fg = palette.fg0 },
-        c = { bg = palette.bg0, fg = palette.comment },
+        b = { bg = palette.bg3, fg = palette.fg1 },
+        c = { bg = palette.bg0, fg = palette.fg0 },
     },
     visual = {
         a = { bg = palette.orange, fg = palette.bg0 },
-        b = { bg = palette.bg4, fg = palette.fg0 },
-        c = { bg = palette.bg0, fg = palette.comment },
+        b = { bg = palette.bg3, fg = palette.fg1 },
+        c = { bg = palette.bg0, fg = palette.fg0 },
     },
     replace = {
         a = { bg = palette.red, fg = palette.bg0 },
-        b = { bg = palette.bg4, fg = palette.fg0 },
-        c = { bg = palette.bg0, fg = palette.comment },
+        b = { bg = palette.bg3, fg = palette.fg1 },
+        c = { bg = palette.bg0, fg = palette.fg0 },
     },
     command = {
         a = { bg = palette.purple, fg = palette.bg0 },
-        b = { bg = palette.bg4, fg = palette.fg0 },
-        c = { bg = palette.bg0, fg = palette.comment },
+        b = { bg = palette.bg3, fg = palette.fg1 },
+        c = { bg = palette.bg0, fg = palette.fg0 },
     },
     terminal = {
         a = { bg = palette.cyan, fg = palette.bg0 },
-        b = { bg = palette.bg4, fg = palette.fg0 },
-        c = { bg = palette.bg0, fg = palette.comment },
+        b = { bg = palette.bg3, fg = palette.fg1 },
+        c = { bg = palette.bg0, fg = palette.fg0 },
     },
     inactive = {
         a = { bg = palette.fg0, fg = palette.bg0 },
-        b = { bg = palette.bg4, fg = palette.fg0 },
-        c = { bg = palette.bg0, fg = palette.comment },
+        b = { bg = palette.bg3, fg = palette.fg1 },
+        c = { bg = palette.bg0, fg = palette.fg0 },
     },
 }
