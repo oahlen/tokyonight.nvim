70a2305920e1b72c14a968cd1ffdd0c35763eb75
Oscar Ahlén <github@ahlen.dev>
2026-05-31 20:51:33 +0200

    Simplify lualine theme

--- lua/lualine/themes/tokyonight.lua
+++ lua/lualine/themes/tokyonight.lua
@@ -1,45 +1,6 @@
-if vim.o.background == "light" then
-    local palette = require("tokyonight.palette-light")
-    return {
-        normal = {
-            a = { bg = palette.blue, fg = palette.bg0 },
-            b = { bg = palette.bg3, fg = palette.fg1 },
-            c = { bg = palette.bg0, fg = palette.fg0 },
-        },
-        insert = {
-            a = { bg = palette.green, fg = palette.bg0 },
-            b = { bg = palette.bg3, fg = palette.fg1 },
-            c = { bg = palette.bg0, fg = palette.fg0 },
-        },
-        visual = {
-            a = { bg = palette.orange, fg = palette.bg0 },
-            b = { bg = palette.bg3, fg = palette.fg1 },
-            c = { bg = palette.bg0, fg = palette.fg0 },
-        },
-        replace = {
-            a = { bg = palette.red, fg = palette.bg0 },
-            b = { bg = palette.bg3, fg = palette.fg1 },
-            c = { bg = palette.bg0, fg = palette.fg0 },
-        },
-        command = {
-            a = { bg = palette.purple, fg = palette.bg0 },
-            b = { bg = palette.bg3, fg = palette.fg1 },
-            c = { bg = palette.bg0, fg = palette.fg0 },
-        },
-        terminal = {
-            a = { bg = palette.cyan, fg = palette.bg0 },
-            b = { bg = palette.bg3, fg = palette.fg1 },
-            c = { bg = palette.bg0, fg = palette.fg0 },
-        },
-        inactive = {
-            a = { bg = palette.fg0, fg = palette.bg0 },
-            b = { bg = palette.bg3, fg = palette.fg1 },
-            c = { bg = palette.bg0, fg = palette.fg0 },
-        },
-    }
-end
+local theme = vim.o.background == "light" and "tokyonight.palette-light" or "tokyonight.palette-dark"
+local palette = require(theme)
 
-local palette = require("tokyonight.palette-dark")
 return {
     normal = {
         a = { bg = palette.blue, fg = palette.bg0 },
